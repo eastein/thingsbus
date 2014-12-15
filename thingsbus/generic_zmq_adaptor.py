1a0ebb5cd82daa371b647fe9b61254de9ceca6d7
Eric Stein <toba@des.truct.org>
2014-12-14 23:29:09 -0600

    fix tabs & make the generic zmq adaptor program work

--- thingsbus/generic_zmq_adaptor.py
+++ thingsbus/generic_zmq_adaptor.py
@@ -15,49 +15,54 @@ projections = luminance,ratio_busy
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
-    parser.add_option('-u', '--url', dest='thingsbus_broker_input_url', default=None, help="ZMQ url to connect to - things bus broker input.")
+    parser.add_option('-u', '--url', dest='thingsbus_broker_input_url', default=None,
+                      help="ZMQ url to connect to - things bus broker input.")
     parser.add_option('-z', '--zone', dest='thingsbus_zone', default=None, help='Thingsbus zone.')
-    parser.add_option('-s', '--sources', dest='source_urls', help="ZMQ urls to connect to (they should be the bind end of PUB/SUB) to receive the messages.")
-    parser.add_option('--tskey', dest='tskey', default=None, help="The key in the messages to use to get the time. Optional. Should be a float of seconds since 1970.")
-    parser.add_option('--nskey', dest='nskey', default=None, help="The key in the messages to use to get the final part of the namespace from. Optional.")
-    parser.add_option('--filter', dest='filter', default=None, help=": separated - the key and the value to check-equals to. Use for limiting the flow of events into things bus.")
-    parser.add_option('--projections', dest='projections', default=None, help='Comma separated list of fields to include in the data dictionary into things bus.')
+    parser.add_option('-s', '--sources', dest='source_urls',
+                      help="ZMQ urls to connect to (they should be the bind end of PUB/SUB) to receive the messages.")
+    parser.add_option('--tskey', dest='tskey', default=None,
+                      help="The key in the messages to use to get the time. Optional. Should be a float of seconds since 1970.")
+    parser.add_option('--nskey', dest='nskey', default=None,
+                      help="The key in the messages to use to get the final part of the namespace from. Optional.")
+    parser.add_option('--filter', dest='filter', default=None,
+                      help=": separated - the key and the value to check-equals to. Use for limiting the flow of events into things bus.")
+    parser.add_option('--projections', dest='projections', default=None,
+                      help='Comma separated list of fields to include in the data dictionary into things bus.')
     parser.add_option('--ns', dest='ns', help='Namespace to put information into')
 
     (opts, args) = parser.parse_args()
 
     sources = list()
-    source = None    
-    for url in opts.source_urls.split(',') :
+    source = None
+    for url in opts.source_urls.split(','):
         source = zmqsub.ConnectSub(url, context=source)
         sources.append(source)
-    
-    adaptor = thingsbus.adaptor.Adaptor(opts.ns, broker_input_url=opts.thingsbus_broker_input_url, zone=opts.thingsbus_zone)
+
+    adaptor = thingsbus.adaptor.Adaptor(
+        opts.ns, broker_input_url=opts.thingsbus_broker_input_url, zone=opts.thingsbus_zone)
     adaptor.start()
 
-    try :
-        while True :
-            for source in sources :
-                try :
+    try:
+        while True:
+            for source in sources:
+                try:
                     msg = source.recv(timeout=0.01)
-    
-                    k,v = opts.filter.split(':', 1)
 
-                    if k not in msg :
-                        continue
-                    if msg[k] != v :
-                        continue
+                    k, v = opts.filter.split(':', 1)
 
-                    # TODO lol support optional tskey nskey
+                    if k not in msg:
+                        continue
+                    if msg[k] != v:
+                        continue
 
                     ns = msg[opts.nskey]
                     ts = msg[opts.tskey]
                     data = dict()
-                    for p in opts.projections.split(',') :
-                        data[p] = msg[p] # haha this really sucks
+                    for p in opts.projections.split(','):
+                        data[p] = msg[p]
 
                     adaptor.send(data, ns=ns, ts=ts)
-                except zmqsub.NoMessagesException :
+                except zmqsub.NoMessagesException:
                     pass
-    except KeyboardInterrupt :
+    except KeyboardInterrupt:
         adaptor.stop()
