7ba20b4d4269a14b91a443bcea8c214fa618dc63
Eric Stein <toba@des.truct.org>
2015-01-17 17:51:07 -0600

    add --namespace option to examples/print_all_events.py

--- examples/print_all_events.py
+++ examples/print_all_events.py
@@ -11,15 +11,22 @@ if __name__ == '__main__':
     parser.add_option('-u', '--url', dest='thingsbus_broker_url', default=None,
                       help="ZMQ url to connect to - things bus broker.")
     parser.add_option('-z', '--zone', dest='thingsbus_zone', default=None, help='Thingsbus zone.')
+    parser.add_option('--namespace', '-n', dest='ns', default=None,
+                      help="Namespace to subscribe to (TREE mode - everything under that as well) - if not given, will use root namespcae.")
 
     (opts, args) = parser.parse_args()
 
     client = thingsbus.client.Client(broker_url=opts.thingsbus_broker_url, zone=opts.thingsbus_zone)
-    
-    def print_e(e) :
+
+    def print_e(e):
         print e
 
-    client.directory.root.subscribe(print_e, thingsbus.client.F_TREE)
+    if opts.ns:
+        thing = client.directory.get_thing(opts.ns)
+    else:
+        thing = client.directory.root
 
-    while True :
+    thing.subscribe(print_e, thingsbus.client.F_TREE)
+
+    while True:
         time.sleep(1)
