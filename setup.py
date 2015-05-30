2ba23a05da2093e8f2531bbd62a7adc3e0a0ecf7
Eric Stein <toba@des.truct.org>
2015-05-30 13:21:49 -0500

    For #11 added six to reqs

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name='thingsbus',
       author_email='toba@des.truct.org',
       url='https://github.com/eastein/thingsbus/',
       packages=['thingsbus'],
-      install_requires=['dnspython', 'zmqfan>=0.7', 'msgpack-python']
+      install_requires=['dnspython', 'zmqfan>=0.7', 'msgpack-python', 'six']
      )
