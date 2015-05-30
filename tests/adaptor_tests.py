7aaaac4e2df3bee0d08df043fa457278be3241a0
Eric Stein <toba@des.truct.org>
2015-05-30 13:21:07 -0500

    Making progress on #11 python3 compatibility. Bad test coverage but python-modernize seems to have helped.

---
+++ tests/adaptor_tests.py
@@ -0,0 +1,7 @@
+import unittest
+import thingsbus.adaptor
+import nose.exc
+
+class AdaptorTests(unittest.TestCase):
+    def test_update_send_nonamespace(self):
+        raise nose.exc.SkipTest("need to write test")
---
+++ tests/servicediscovery_tests.py
@@ -0,0 +1,7 @@
+import unittest
+import thingsbus.service_discovery
+import nose.exc
+
+class SDTests(unittest.TestCase):
+    def test_srv_record_requesting(self):
+        raise nose.exc.SkipTest("need to write test")
